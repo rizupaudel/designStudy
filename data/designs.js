f992d7268fc4604cf83fabbdc718dc27e9e33e7d
rizupaudel <rizu.paudel1@gmail.com>
2022-12-20 12:15:31 -0700

    Analysis

--- data/designs.js
+++ data/designs.js
@@ -1,29 +1,29 @@
 var designs = { 
     1: {
-        name: "Log",
-        images: ["Logos1.png", "Logos2.png"],
+        name: "Inf",
+        images: ["Infographics.png"],
         csquest: [[1], [2, 3, 4], [6, 8], [9, 10]]
     },
     2: {
-        name: "Dar",
-        images: ["Dart1.png", "Dart2.png"],
-        csquest: [[1], [2, 3, 4], [5, 6, 7, 8], [9, 10, 11]]
-    },
-    3: {
-        name: "Per",
-        images: ["Personal1.png", "Personal2.png", "Personal3.png"],
-        csquest: [[1], [2, 3, 4], [5, 6, 7, 8], [9, 10]]
-    },
-    4: {
-        name: "Pro",
-        images: ["Professional1.png", "Professional2.png", "Professional3.png"],
-        csquest: [[1], [2, 3, 4], [5, 6, 7, 8], [9, 10]]
-    },
-    5: {
         name: "Con",
-        images: ["Consequences1.png", "Consequences2.png"],
-        csquest: [[1], [2, 3, 4], [5, 6, 7, 8], [9, 10]]
+        images: ["Conversation1.png", "Conversation2.png", "Conversation3.png", "Conversation4.png", "Conversation5.png", "Conversation6.png", "Conversation7.png", "Conversation8.png", "Conversation9.png", "Conversation10.png", "Conversation11.png", "Conversation12.png", "Conversation13.png", "Conversation14.png"],
+        csquest: [[1], [2, 3, 4], [5, 6, 7, 8], [9, 10, 11]]
     }
+    // 3: {
+    //     name: "Per",
+    //     images: ["Personal1.png", "Personal2.png", "Personal3.png"],
+    //     csquest: [[1], [2, 3, 4], [5, 6, 7, 8], [9, 10]]
+    // },
+    // 4: {
+    //     name: "Pro",
+    //     images: ["Professional1.png", "Professional2.png", "Professional3.png"],
+    //     csquest: [[1], [2, 3, 4], [5, 6, 7, 8], [9, 10]]
+    // },
+    // 5: {
+    //     name: "Con",
+    //     images: ["Consequences1.png", "Consequences2.png"],
+    //     csquest: [[1], [2, 3, 4], [5, 6, 7, 8], [9, 10]]
+    // }
 }
 
 exports.designs = designs;
--- data/questions/csquest.js
+++ data/questions/csquest.js
@@ -1,7 +1,7 @@
 var questions = [
     {
         "qid": 1,
-        "text": "The way the design portrays what could happen because of the password strength is:",
+        "text": "The way the design presents a password creation strategy is:",
         "subquestions": [
             {
                 "sid": 1,
@@ -94,19 +94,10 @@ var questions = [
     },
     {
         "qid": 3,
-        "text": "The way the design portrays what could happen because of the password strength:",
+        "text": "The presented design:",
         "subquestions": [
             {
                 "sid": 1,
-                "type": "likert",
-                "title": "Makes me worried",
-                "elements": {
-                    "low": "Strongly Disagree",
-                    "high": "Strongly Agree"
-                }
-            },
-            {
-                "sid": 2,
                 "type": "likert",
                 "title": "Encourages me to create a strong password",
                 "elements": {
@@ -115,18 +106,18 @@ var questions = [
                 }
             },
             {
-                "sid": 3,
+                "sid": 2,
                 "type": "likert",
-                "title": "Makes me feel emotionally connected to the presented scenario",
+                "title": "Makes me feel capable of creating a strong password ",
                 "elements": {
                     "low": "Strongly Disagree",
                     "high": "Strongly Agree"
                 }
             },
             {
-                "sid": 4,
+                "sid": 3,
                 "type": "likert",
-                "title": "Makes me feel personally connected to the presented scenario",
+                "title": "Makes me feel confident that I can create a strong password",
                 "elements": {
                     "low": "Strongly Disagree",
                     "high": "Strongly Agree"
@@ -136,7 +127,7 @@ var questions = [
     },
     {
         "qid": 4,
-        "text": "It is easy for me to follow the message depicted across multiple pages in the design.",
+        "text": "The way the design communicates a password creation strategy to me makes me feel personally connected.",
         "subquestions": [
             {
                 "sid": 1,
@@ -151,7 +142,7 @@ var questions = [
     },
     {
         "qid": 5,
-        "text": "In the presented design, the portrayal of hacker stimulates me to create a strong password.",
+        "text": "It is easy for me to follow the message depicted across multiple pages in the design.",
         "subquestions": [
             {
                 "sid": 1,
@@ -181,7 +172,40 @@ var questions = [
     },
     {
         "qid": 7,
-        "text": "In the presented design, the portrayal of victim encourages me to create a strong password.",
+        "text": "In this design, the portrayal of a security expert:",
+        "subquestions": [
+            {
+                "sid": 1,
+                "type": "likert",
+                "title": "Stimulates me to create a strong password",
+                "elements": {
+                    "low": "Strongly Disagree",
+                    "high": "Strongly Agree"
+                }
+            },
+            {
+                "sid": 2,
+                "type": "likert",
+                "title": "Makes me put trust on the presented password creation strategy",
+                "elements": {
+                    "low": "Strongly Disagree",
+                    "high": "Strongly Agree"
+                }
+            },
+            {
+                "sid": 3,
+                "type": "likert",
+                "title": "Encourages me to use the presented password creation strategy in real life",
+                "elements": {
+                    "low": "Strongly Disagree",
+                    "high": "Strongly Agree"
+                }
+            }
+        ]
+    },
+    {
+        "qid": 8,
+        "text": "In this design, the illustration of an example helped me to better understand the password creation strategy.",
         "subquestions": [
             {
                 "sid": 1,
@@ -195,7 +219,7 @@ var questions = [
         ]
     },
     {
-        "qid": 8,
+        "qid": 9,
         "text": "In which type website's account creation page would you want to see this design?",
         "subquestions": [
             {
@@ -218,7 +242,7 @@ var questions = [
         ]
     },
     {
-        "qid": 9,
+        "qid": 10,
         "text": "If this design is implemented in real life and shown to me before I create a password for an online account I care about, I will be motivated to create a stronger password than the one I would have created if the design were not presented to me during the sign up.",
         "subquestions": [
             {
@@ -233,8 +257,8 @@ var questions = [
         ]
     },
     {
-        "qid": 10,
-        "text": "Which parts of the presented design motivates you most to create a strong password?",
+        "qid": 11,
+        "text": "How do you evaluate the presentation of this design in conveying a password creation strategy to users?",
         "subquestions": [
             {
                 "sid": 1,
@@ -244,8 +268,8 @@ var questions = [
         ]
     },
     {
-        "qid": 11,
-        "text": "What do you think the dartboard implies here in the design? Please explain in 2-3 sentences.",
+        "qid": 12,
+        "text": "In this multi-page design, the page on the left made me reflect on how I am doing in following the presented password creation strategy.",
         "subquestions": [
             {
                 "sid": 1,
