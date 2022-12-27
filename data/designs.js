1c38a998ff48691465bd416cbe13a4e04cc8ab51
rizupaudel <rizu.paudel1@gmail.com>
2022-12-27 13:57:20 -0700

    Reflection design one data

--- data/designs.js
+++ data/designs.js
@@ -8,7 +8,7 @@ var designs = {
     2: {
         name: "Con",
         images: ["Conversation1.png", "Conversation2.png", "Conversation3.png", "Conversation4.png", "Conversation5.png", "Conversation6.png", "Conversation7.png", "Conversation8.png", "Conversation9.png", "Conversation10.png", "Conversation11.png", "Conversation12.png", "Conversation13.png", "Conversation14.png"],
-        maxcount: 150,
+        maxcount: 0,
         csquest: [[1], [2, 3, 4, 5], [6, 7, 8, 9], [10, 11]]
     },
     3: {
@@ -20,7 +20,7 @@ var designs = {
     4: {
         name: "Ref",
         images: ["Reflection1.png", "Reflection2.png", "Reflection3.png", "Reflection4.png", "Reflection5.png", "Reflection6.png", "Reflection7.png"],
-        maxcount: 0,
+        maxcount: 150,
         csquest: [[1], [2, 3, 4, 5], [6, 7, 8, 9], [10, 11], [12]]
     }
 }
