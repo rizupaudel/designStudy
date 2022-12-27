70ee157d59017cfd2161e9ca9abafa3c67a96c5f
rizupaudel <rizu.paudel1@gmail.com>
2022-12-27 12:08:27 -0700

    Two participants for conversation design

--- data/designs.js
+++ data/designs.js
@@ -2,13 +2,13 @@ var designs = {
     1: {
         name: "Inf",
         images: ["Infographics.png"],
-        maxcount: 150,
+        maxcount: 0,
         csquest: [[1], [2, 3, 4, 13], [6, 7, 8, 9], [10, 11]]
     },
     2: {
         name: "Con",
         images: ["Conversation1.png", "Conversation2.png", "Conversation3.png", "Conversation4.png", "Conversation5.png", "Conversation6.png", "Conversation7.png", "Conversation8.png", "Conversation9.png", "Conversation10.png", "Conversation11.png", "Conversation12.png", "Conversation13.png", "Conversation14.png"],
-        maxcount: 0,
+        maxcount: 150,
         csquest: [[1], [2, 3, 4, 5], [6, 7, 8, 9], [10, 11]]
     },
     3: {
