90ba7d44d6e880e898726d1335ac04b9ebf2a29f
rizupaudel <rizu.paudel1@gmail.com>
2022-12-26 19:31:53 -0700

    guidance for first three designs

--- data/designs.js
+++ data/designs.js
@@ -2,26 +2,26 @@ var designs = {
     1: {
         name: "Inf",
         images: ["Infographics.png"],
-        maxcount: 26,
+        maxcount: 100,
         csquest: [[1], [2, 3, 4, 13], [6, 7, 8, 9], [10, 11]]
 
     },
     2: {
         name: "Con",
         images: ["Conversation1.png", "Conversation2.png", "Conversation3.png", "Conversation4.png", "Conversation5.png", "Conversation6.png", "Conversation7.png", "Conversation8.png", "Conversation9.png", "Conversation10.png", "Conversation11.png", "Conversation12.png", "Conversation13.png", "Conversation14.png"],
-        maxcount: 26,
+        maxcount: 100,
         csquest: [[1], [2, 3, 4, 5], [6, 7, 8, 9], [10, 11]]
     },
     3: {
         name: "Onl",
         images: ["OnlineChat.png"],
-        maxcount: 26,
+        maxcount: 100,
         csquest: [[1], [2, 3, 4, 13], [6, 7, 8, 9], [10, 11]]
     },
     4: {
         name: "Ref",
         images: ["Reflection1.png", "Reflection2.png", "Reflection3.png", "Reflection4.png", "Reflection5.png", "Reflection6.png", "Reflection7.png"],
-        maxcount: 26,
+        maxcount: 0,
         csquest: [[1], [2, 3, 4, 5], [6, 7, 8, 9], [10, 11], [12]]
     }
 }
