e97f87719d70a1f242eba7c7a21f17f6c37ec2ee
rizupaudel <rizu.paudel1@gmail.com>
2023-12-06 16:58:37 -0700

    Updated designs and info

--- data/questions/spasssurvey.js
+++ data/questions/spasssurvey.js
@@ -1,8 +1,8 @@
 var questions = [
     {
         "qid": 1,
-        "text": "Let’s say you just got a new game to play on the computer, but you need a password to use it." + 
-        "Please make up a new password using the technique you have just learnt." + 
+        "text": "Let’s say you just got a new game to play on the computer, but you need a password to use it. " + 
+        "Please make up a new password using the technique you have just learnt. " + 
         "(Remember don’t write down one of your real passwords)",
         "subquestions": [
             {
