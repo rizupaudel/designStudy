87e8156058ee4d4894b7a3466a6b1021a5376d25
rizupaudel <rizu.paudel1@gmail.com>
2023-12-18 16:07:35 -0700

    Changed design order Bas-Cus-Sto

--- data/designs.js
+++ data/designs.js
@@ -1,19 +1,19 @@
 var designs = { 
     1: {
+        name: "Bas",
+        images: ["Baseline.png"],
+        maxcount: 0,
+        csquest: [[1], [2, 3, 4], [5], [6, 7, 8], [9, 10, 11], [12, 13]]
+    },
+    2: {
         name: "Cus",
         images: ["Customized1.png", "Customized2.png"],
         maxcount: 0,
         csquest: [[1], [2, 3, 4], [5], [6, 7, 8], [9, 10, 11], [12, 13]]
     },
-    2: {
+    3: {
         name: "Sto",
         images: ["Story1.png", "Story2.png", "Story3.png", "Story4.png", "Story5.png", "Story6.png", "Story7.png", "Story8.png", "Story9.png"],
-        maxcount: 0,
-        csquest: [[1], [2, 3, 4], [5], [6, 7, 8], [9, 10, 11], [12, 13]]
-    },
-    3: {
-        name: "Bas",
-        images: ["Baseline.png"],
         maxcount: 0,
         csquest: [[1], [2, 3, 4], [5], [6, 7, 8], [9, 10, 11], [12, 13]]
     }
